8245cd3469ada3bd315a7421f0cb3a656e3a033c
Lee <114220313@umail.ucc.ie>
2020-09-03 12:46:36 +0100

    Initial Commit

---
+++ BeersApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeersApp
+//
+//  Created by 李祺 on 03/09/2020.
+//  Copyright © 2020 Drop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
